13900a504d26141a761efd84e5b18f12a6ec7635
budi1710 <budbudutomo17@gmail.com>
2021-06-12 21:56:58 +0800

    Membuat Variable

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Budi";
+        String namaBelakang = "Utomo";
+        int usia = 27;
+        int targetTahunKuliah = 4;
+        double ipk = 3.3;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println( namaDepan + namaBelakang );
+    }
+}
